78e11269e90dfa638fd3dd9e6c28dda61d5c0e07
sunnyraj <mrsj5714@gmail.com>
2026-07-15 18:07:35 +0530

    hackerrank sql practice

---
+++ HackerRank_1.sql
@@ -0,0 +1,3 @@
+SELECT * FROM CITY WHERE COUNTRYCODE = 'USA' AND POPULATION > 100000;
+
+-- https://www.hackerrank.com/challenges/revising-the-select-query/problem?isFullScreen=true
---
+++ HackerRank_2.sql
@@ -0,0 +1,4 @@
+SELECT NAME FROM CITY WHERE COUNTRYCODE = 'USA' AND POPULATION > 120000;
+
+
+-- https://www.hackerrank.com/challenges/revising-the-select-query-2/problem?isFullScreen=true
