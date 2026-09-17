87f4650f250e25b076f22fc2e31b23b9938448a8
sunnyraj <mrsj5714@gmail.com>
2026-09-17 23:22:04 +0530

    hackerrank problem added

---
+++ HRank16.sql
@@ -0,0 +1,11 @@
+-- https://www.hackerrank.com/challenges/weather-observation-station-11/problem?isFullScreen=true
+
+
+/*
+    Enter your query here and follow these instructions:
+    1. Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+    2. The AS keyword causes errors, so follow this convention: "Select t.Field From table1 t" instead of "select t.Field From table1 AS t"
+    3. Type your code immediately after comment. Don't leave any blank line.
+*/
+
+SELECT DISTINCT CITY FROM STATION WHERE UPPER(SUBSTR(CITY, 1, 1)) NOT IN ('A', 'E', 'I', 'O', 'U') OR UPPER(SUBSTR(CITY, LENGTH(CITY), 1)) NOT IN ('A', 'E', 'I', 'O', 'U');
